5b4d953e94751970611f0cdf4520df5ec6b1b44c
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-10-07 17:12:35 -0400

    Add a hash to TextAsset

--- AssetRipper.Mining.PredefinedAssets/TextAsset.cs
+++ AssetRipper.Mining.PredefinedAssets/TextAsset.cs
@@ -1,4 +1,8 @@
-﻿namespace AssetRipper.Mining.PredefinedAssets;
+﻿using System.Buffers.Binary;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace AssetRipper.Mining.PredefinedAssets;
 
 public sealed record class TextAsset : NamedObject
 {
@@ -6,4 +10,31 @@ public sealed record class TextAsset : NamedObject
 	/// The length the text in bytes.
 	/// </summary>
 	public required int Length { get; init; }
+
+	/// <summary>
+	/// An MD5 hash of the text contents.
+	/// </summary>
+	public required UInt128 Hash { get; init; }
+
+	public TextAsset()
+	{
+	}
+
+	[SetsRequiredMembers]
+	public TextAsset(string name, ReadOnlySpan<byte> data)
+	{
+		Name = name;
+		Length = data.Length;
+		Hash = HashData(data);
+	}
+
+	private static UInt128 HashData(ReadOnlySpan<byte> data)
+	{
+		Span<byte> buffer = stackalloc byte[16];
+		MD5.HashData(data, buffer);
+		return new UInt128(
+			BinaryPrimitives.ReadUInt64LittleEndian(buffer[8..]),
+			BinaryPrimitives.ReadUInt64LittleEndian(buffer));
+		//On .NET 8, replace with ReadUInt128LittleEndian
+	}
 }
