c009d0cab9b3e4976bbdf95f1b41e566b1fac9f5
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-01-15 20:36:30 -0500

    Use string version in UnityPackageData and bump version

--- AssetRipper.Mining.PredefinedAssets/UnityPackageData.cs
+++ AssetRipper.Mining.PredefinedAssets/UnityPackageData.cs
@@ -10,14 +10,14 @@ namespace AssetRipper.Mining.PredefinedAssets;
 /// File extensions are not included in any names.
 /// </remarks>
 /// <param name="Name">The name of this Unity package.</param>
-/// <param name="Version">The version of this Unity package. Only applicable if <paramref name="UsedInPackageJson"/> is true.</param>
+/// <param name="Version">The version or git url of this Unity package. Only applicable if <paramref name="UsedInPackageJson"/> is true.</param>
 /// <param name="UsedInPackageJson">If true, this Unity package should be referenced in the project's package json file.</param>
 /// <param name="Assemblies">A dictionary of compiled dll assemblies in the package.</param>
 /// <param name="Scripts">A dictionary of loose cs scripts in the package. Assembly name : { Full name : Guid }</param>
 /// <param name="Assets">A dictionary of assets in the package.</param>
 public readonly record struct UnityPackageData(
 	string Name,
-	Version Version,
+	string Version,
 	bool UsedInPackageJson,
 	Dictionary<string, UnityGuid> Assemblies,
 	Dictionary<string, Dictionary<string, UnityGuid>> Scripts,
