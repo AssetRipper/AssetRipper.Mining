0686d2c451b94f7effe1f3040bce1042b597fb2c
aldelaro5 <aldelaro5@gmail.com>
2024-05-30 23:35:12 -0400

    Add the AABB fields in the key of meshes

--- AssetRipper.Mining.PredefinedAssets/Mesh.cs
+++ AssetRipper.Mining.PredefinedAssets/Mesh.cs
@@ -4,15 +4,17 @@ public sealed record class Mesh : NamedObject
 {
 	public required int VertexCount { get; init; }
 	public required int SubMeshCount { get; init; }
+	public required AxisAlignedBoundingBox LocalAABB { get; init; }
 
 	public Mesh()
 	{
 	}
 
 	[SetsRequiredMembers]
-	public Mesh(string name, int vertexCount, int subMeshCount) : base(name)
+	public Mesh(string name, int vertexCount, int subMeshCount, AxisAlignedBoundingBox localAabb) : base(name)
 	{
 		VertexCount = vertexCount;
 		SubMeshCount = subMeshCount;
+		LocalAABB = localAabb;
 	}
 }
